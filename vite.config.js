f96caa9a80bbf5e34a4dd411a67711f15e1a6b1c
mrouw <bchva@yandex.ru>
2024-05-08 19:51:15 +0700

    fixed vite.config

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,6 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/project_1',
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
